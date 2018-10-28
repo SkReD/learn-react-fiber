012866b473a7fcb87b041a6723e0580e70e85a1d
Mikhail <mshipov.npm@yandex.ru>
2018-10-28 12:47:17 +0300

    add initial files and configurations

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    // configuration regarding modules
+    rules: [
+      // rules for modules (configure loaders, parser options, etc.)
+      {
+        test: /\.js?$/,
+        exclude: [
+          path.resolve(__dirname, "node_modules")
+        ],
+        // flags to apply these rules, even if they are overridden (advanced option)
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
